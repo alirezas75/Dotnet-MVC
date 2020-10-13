a7b0e09ed8b70338040e353eb6a776253ab19af7
alireza <alireza.soleimani75@yahoo.com>
2020-10-13 15:23:46 +0330

    Dotnet MVC Created

---
+++ Controllers/BookController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetWebApp.Controllers
+{
+    public class BookController : Controller
+    {
+        public string GetAllBooks()
+        {
+            return "All books";
+        }
+
+        public string GetBook(int id)
+        {
+            return $"Book with id = {id}";
+        }
+
+        public string SearchBooks(string bookName, string authorName)
+        {
+            return $"Book name = {bookName} & Author = {authorName}";
+        }
+    }
+}
